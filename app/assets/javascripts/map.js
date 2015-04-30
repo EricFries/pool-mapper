8f4163b0d10b3de08480ea9a20db0591f64014a7
EricFries <ericfries@gmail.com>
2015-04-30 19:58:46 -0400

    refactor javascript map code

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,6 @@
+function drawMap(){
+	var mapOptions = {
+	center: { lat: 40.7033127, lng: -73.979681},
+	zoom: 11
+	};
+}
--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,8 +1,6 @@
 class PoolsController < ApplicationController
   def index
     @pools = Pool.get_pools
-    # respond_to do |format|
-    #   format.js
-    # end
+
   end
 end
