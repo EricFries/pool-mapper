cd7101be50f1c2f6a765465255bc983279bda797
EricFries <ericfries@gmail.com>
2015-04-30 17:38:43 -0400

    make listeners dynamic

---
+++ config/initializers/syncano.rb
@@ -0,0 +1,7 @@
+# Syncano instance name
+SYNCANO_INSTANCE_NAME = 'morning-violet-433730'
+# Syncano api key
+SYNCANO_API_KEY = 'f8c7f8700a4bcc1cf272ad775865d725a3482380'
+
+# Collection used for Syncano::ActiveRecord
+SYNCANO_ACTIVERECORD_COLLECTION = Syncano.client.projects.first.collections.first
